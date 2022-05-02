13baf10a892aca9a6787828c0824d7073cd0a814
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-01 22:36:02 -0300

    Migração Persons

---
+++ src/database/typeorm/migrations/1651436349061-user.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class user1651436349061 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'persons',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '250',
+            isNullable: false,
+          },
+          {
+            name: 'birthAt',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '16',
+            isNullable: true,
+          },
+          {
+            name: 'document',
+            type: 'varchar',
+            length: '14',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'datetime',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'datetime',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('persons');
+  }
+}
