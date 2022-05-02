c78345e4c4df0672d467991e05fddbdb959e0a31
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-02 14:47:40 -0300

    Criando Modulo e Serviço Prisma ORM Código serviço vem da documentação Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import * as typeOrmConfig from '../database/typeorm/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PrismaModule],
   controllers: [AppController],
   providers: [],
 })
