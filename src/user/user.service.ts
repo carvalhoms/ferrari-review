8904955c0a2b0ea0d99a372d5f174a10f4ea12c4
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-02 15:27:25 -0300

    Buscando usuário no banco pelo id Usuário unico pelo id ou email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async get(id: number) {
+    // Verificando se o id é numérico, se não for exibe erro
+    if (isNaN(Number(id))) {
+      throw new BadRequestException('Id is required');
+    }
+
+    // Busca usuário no banco pelo id
+    // findUnique traz usuário único encontrado
+    const user = await this.prisma.users.findUnique({
+      // where busca usuário pelo id
+      where: {
+        id,
+      },
+      // include busca persons relacionados ao usuário obtido pelo whare
+      include: {
+        persons: true,
+      },
+    });
+
+    // Verifica se existe algum usuário, se não existir exibe erro
+    if (!user) {
+      throw new BadGatewayException('User not found');
+    }
+
+    // Retornando usuário encontrado
+    return user;
+  }
 }
