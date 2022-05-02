c559346a9981eab7235a242c30b1f70f643893f4
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-02 17:06:29 -0300

    Criando Auth e fazendo as importações e injeções

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import * as typeOrmConfig from '../database/typeorm/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PrismaModule, UserModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PrismaModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
