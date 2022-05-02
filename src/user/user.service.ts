511647add4788aa8a6db07a0908b336b1633cbdb
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-02 16:24:38 -0300

    Criando método para buscar usuario pelo email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -39,4 +39,32 @@ export class UserService {
     // Retornando usuário encontrado
     return user;
   }
+
+  async getByEmail(email: string) {
+    // Verificando se o email existe
+    if (!email) {
+      throw new BadRequestException('Email is required');
+    }
+
+    // Busca usuário no banco pelo email
+    // findUnique traz usuário único encontrado
+    const user = await this.prisma.users.findUnique({
+      // where para buscar usuário pelo email
+      where: {
+        email,
+      },
+      // include busca persons relacionados ao usuário obtido pelo whare
+      include: {
+        persons: true,
+      },
+    });
+
+    // Verifica se existe algum usuário, se não existir exibe erro
+    if (!user) {
+      throw new BadGatewayException('User not found');
+    }
+
+    // Retornando usuário encontrado
+    return user;
+  }
 }
