51fd13fa06948bdaddf0c981601829f65f2114ef
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-06 14:43:46 -0300

    Configuração Mailer module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,26 @@
 import { MailService } from './mail.service';
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async () => ({
+        transport: {
+          host: process.env.MAIL_HOST,
+          port: Number(process.env.MAIL_PORT),
+          secure: Boolean(process.env.MAIL_SECURE),
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+          },
+        },
+        defaults: {
+          from: `Ferrari review <${process.env.MAIL_FROM}>`,
+        },
+      }),
+    }),
+  ],
   controllers: [],
   providers: [MailService],
 })
