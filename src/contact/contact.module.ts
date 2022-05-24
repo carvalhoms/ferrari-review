f37a3c25c011546411585c04d525e0b619de921c
Marcelo Carvalho <carvalhoms@icloud.com>
2022-05-24 19:59:00 -0300

    Criando service Contacts

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,9 +1,10 @@
+import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [],
   controllers: [ContactController],
-  providers: [],
+  providers: [ContactService],
 })
 export class ContactModule {}
